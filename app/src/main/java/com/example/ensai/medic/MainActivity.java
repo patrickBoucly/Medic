8e62c043e6621720363bd15282b7cae3fe71ada1
patrick boucly <patrick.boucly@gmail.com>
2017-05-09 15:07:17 +0200

    menus fait

--- app/src/main/java/com/example/ensai/medic/MainActivity.java
+++ app/src/main/java/com/example/ensai/medic/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.ensai.medic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    private Button buttonPharma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonPharma=(Button) findViewById((R.id.menu_pharma));
     }
 }
